47d912460dd4da7a86cd7a5d7dc0c4b30889518f
dpozinen <dpozinen@gmail.com>
2022-11-17 21:10:05 +0200

    change advertised listener

--- src/main/kotlin/dpozinen/tracker/stats/kafka/KafkaConfig.kt
+++ src/main/kotlin/dpozinen/tracker/stats/kafka/KafkaConfig.kt
@@ -6,24 +6,25 @@ import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.kafka.support.serializer.JsonDeserializer
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
 
 
-//@EnableKafka
+@EnableKafka
 @Configuration
 class KafkaConfig {
 
     @Bean
-    fun consumerFactory(@Value("\${stats.kafka.address}") kafkaAddress: String) =
+    fun consumerFactory(@Value("\${kafka.address}") kafkaAddress: String) =
         DefaultKafkaConsumerFactory<String, String>(
             mapOf<String, Any>(
                 BOOTSTRAP_SERVERS_CONFIG to kafkaAddress,
                 GROUP_ID_CONFIG to "jormungandr",
                 KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
-                VALUE_DESERIALIZER_CLASS_CONFIG to JsonDeserializer<List<DataPoint>>(),
+                VALUE_DESERIALIZER_CLASS_CONFIG to JsonDeserializer::class.java,
             )
         )
 
