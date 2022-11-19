3291fbf510b56e783a07a846d8ec28d384ed600f
dpozinen <dpozinen@gmail.com>
2022-11-19 15:05:57 +0200

    add persistent logs

--- src/main/kotlin/dpozinen/tracker/stats/influx/InfluxService.kt
+++ src/main/kotlin/dpozinen/tracker/stats/influx/InfluxService.kt
@@ -4,14 +4,20 @@ import com.influxdb.client.domain.WritePrecision
 import com.influxdb.client.kotlin.WriteKotlinApi
 import dpozinen.tracker.stats.domain.DataPoint
 import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.runBlocking
+import mu.KotlinLogging.logger
 import org.springframework.stereotype.Service
 
 @Service
 class InfluxService(private val writeKotlinApi: WriteKotlinApi) {
 
+    private val log = logger {}
+
     fun write(stats: List<DataPoint>) {
         val measurements = stats.map { toMeasurement(it) }.asFlow()
+
+        log.info { "Received measurements ${measurements.map {  }}" }
 
         runBlocking {
             writeKotlinApi.writeMeasurements(measurements, WritePrecision.S)
