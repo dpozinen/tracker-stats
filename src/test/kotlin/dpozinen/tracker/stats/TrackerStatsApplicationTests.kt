29ddea64087cc4f721fb7f48b6c7ab0bd8a4d14a
Dariy Pozinenko <33901469+dpozinen@users.noreply.github.com>
2023-10-30 12:58:53 +0200

    influx test (#11)
    * influx test
    
    * 1.0.1

--- src/test/kotlin/dpozinen/tracker/stats/TrackerStatsApplicationTests.kt
+++ src/test/kotlin/dpozinen/tracker/stats/TrackerStatsApplicationTests.kt
@@ -1,21 +1,30 @@
 package dpozinen.tracker.stats
 
+import com.influxdb.client.kotlin.QueryKotlinApi
 import dpozinen.tracker.stats.domain.DataPoint
 import dpozinen.tracker.stats.postgres.TorrentRepository
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.data.Index.atIndex
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.ConfigurableEnvironment
+import org.springframework.core.env.MapPropertySource
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.test.context.ActiveProfiles
+import org.testcontainers.containers.InfluxDBContainer
 import org.testcontainers.shaded.org.awaitility.Awaitility.await
+import org.testcontainers.utility.DockerImageName
 import java.time.Duration
 import java.time.Instant.now
 import java.time.temporal.ChronoUnit
 
-@SpringBootTest
+@SpringBootTest(classes = [TrackerStatsApplication::class, TestConfig::class])
 @ActiveProfiles("test")
 class TrackerStatsApplicationTests {
 
@@ -26,25 +35,31 @@ class TrackerStatsApplicationTests {
     private lateinit var torrentRepository: TorrentRepository
 
     @Autowired
+    private lateinit var influx: QueryKotlinApi
+
+    @Value("\${influx.org}")
+    private lateinit var org: String
+
+    @Autowired
     private lateinit var kafkaTemplate: KafkaTemplate<String, Any>
 
     @Test
     fun `should save stats`() {
         val now = now()
-		val dataPoint = DataPoint(
-			torrentId = "822a16435d53f9f712f747eef53d693cfeb52768",
-			name = "The Good Shepherd (2006) (1080p BluRay x265 Panda).mkv",
-			size = 123,
-			dateAdded = now,
-			upSpeed = 1,
-			downSpeed = 2,
-			uploaded = 3,
-			downloaded = 4,
-			timestamp = now,
-		)
+        val dataPoint = DataPoint(
+            torrentId = "822a16435d53f9f712f747eef53d693cfeb52768",
+            name = "The Good Shepherd (2006) (1080p BluRay x265 Panda).mkv",
+            size = 123,
+            dateAdded = now,
+            upSpeed = 1,
+            downSpeed = 2,
+            uploaded = 3,
+            downloaded = 4,
+            timestamp = now,
+        )
 
         await().atMost(Duration.ofSeconds(60)).untilAsserted {
-		    kafkaTemplate.send(topic, listOf(dataPoint)).get()
+            kafkaTemplate.send(topic, listOf(dataPoint)).get()
 
             assertThat(torrentRepository.findAll())
                 .hasSize(1)
@@ -56,6 +71,61 @@ class TrackerStatsApplicationTests {
                     assertThat(it.uploaded).isEqualTo(3)
                 }, atIndex(0))
         }
+
+        fun assertTorrentId(values: Map<String, Any>) {
+            assertThat(values).extractingByKey("torrentId")
+                .isEqualTo("822a16435d53f9f712f747eef53d693cfeb52768")
+        }
+
+        runBlocking {
+            influx.query("""from(bucket: "torrents") |> range(start: -1d)""", org)
+                .consumeEach { record ->
+                    assertThat(record).satisfiesAnyOf({
+                        assertThat(it.field).isEqualTo("downloaded")
+                        assertThat(it.value).isEqualTo(4L)
+                        assertTorrentId(it.values)
+                    }, {
+                        assertThat(it.field).isEqualTo("uploaded")
+                        assertThat(it.value).isEqualTo(3L)
+                        assertTorrentId(it.values)
+                    }, {
+                        assertThat(it.field).isEqualTo("downSpeed")
+                        assertThat(it.value).isEqualTo(2L)
+                        assertTorrentId(it.values)
+                    }, {
+                        assertThat(it.field).isEqualTo("upSpeed")
+                        assertThat(it.value).isEqualTo(1L)
+                        assertTorrentId(it.values)
+                    })
+                }
+        }
     }
 
+}
+
+@Configuration
+open class TestConfig {
+
+    @Bean(destroyMethod = "stop")
+    open fun influx(
+        @Value("\${influx.bucket}") bucket: String,
+        @Value("\${influx.token}") token: String,
+        @Value("\${influx.org}") org: String,
+        environment: ConfigurableEnvironment
+    ): InfluxDBContainer<out InfluxDBContainer<*>>? {
+        val influx = InfluxDBContainer(DockerImageName.parse("influxdb:2.0.7"))
+            .withBucket(bucket)
+            .withAdminToken(token)
+            .withOrganization(org)
+        influx.start()
+
+        environment.propertySources.addFirst(
+            MapPropertySource(
+                "influxInfo",
+                mapOf("embedded.influx.url" to influx.url)
+            )
+        )
+
+        return influx
+    }
 }
